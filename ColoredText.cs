46ee5dbd9aea91bccf3860d205ec0937d1078a36
PavelBunos <pavel.bunos@gmail.com>
2022-08-06 14:05:11 +0300

    init: first commit!

---
+++ ColoredText.cs
@@ -0,0 +1,31 @@
+﻿//My first training program on C#.
+//This program paints all characters from your text in random colors.
+//Have fun! :D
+
+using System;
+
+namespace ConsoleApp2 {
+    class ColoredText
+    {
+        static void Main()
+        {
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            
+            Console.WriteLine("Enter any text...");
+            String txt = Console.ReadLine();
+            
+            Console.Clear();
+            Console.Write("Your new painted text: ");
+            foreach(char character in txt)
+            {
+                Random rand = new Random();
+                Console.ForegroundColor = colors[rand.Next(colors.Length)];
+                Console.Write(character);
+            }
+
+            Console.ResetColor();
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+My first training program on C#.  
+This program paints all characters from your text in random colors.  
+Have fun! :D
